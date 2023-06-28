e1674c84a901bd02969904438bc4b6c4145fae0e
SP!RAL <mateuslimarod2602@gmail.com>
2023-06-28 08:33:53 -0300

    first commit

---
+++ routes/professores.js
@@ -0,0 +1,96 @@
+
+const professorServiceMongo = require("../services/professor.service.mongo")
+
+var express = require('express');
+var router = express.Router();
+
+/*router.get(
+    "/listar"
+    ,
+    (req,res,next)=>{
+        res.json(professorService.list())
+    }
+)*/
+
+router.get(
+    "/listar"
+    ,
+    (req,res,next)=>{
+        professorServiceMongo.list(req,res)
+    }
+)
+
+/*router.post(
+    "/cadastrar"
+    ,
+    (req,res,next)=>{
+        //res.json(professorService.list())
+        const professor = professorService.register(req.body)
+        res.json(professor)
+    }
+)*/
+
+router.post(
+    "/cadastrar"
+    ,
+    (req,res,next)=>{
+       professorServiceMongo.register(req,res)
+    }
+)
+
+/*router.get(
+    "/recuperar/:id"
+    ,
+    (req,res,next)=>{
+        //res.json(professorService.list())
+        const professor = professorService.retrieve(req.params.id)
+        res.json(professor)
+    }
+)*/
+
+router.get(
+    "/recuperar/:id"
+    ,
+    (req,res,next)=>{
+       professorServiceMongo.retrieve(req,res)
+    }
+)
+
+/*router.put(
+    "/atualizar/:id"
+    ,
+    (req,res,next)=>{
+        //res.json(professorService.list())
+        const professor = professorService.update(req.params.id,req.body)
+        res.json(professor)
+    }
+)*/
+
+router.put(
+    "/atualizar/:id"
+    ,
+    (req,res,next)=>{
+        professorServiceMongo.update(req,res)
+    }
+)
+
+/*router.delete(
+    "/remover/:id"
+    ,
+    (req,res,next)=>{
+        //res.json(professorService.list())
+        const out = professorService.delete(req.params.id)
+        res.json({"sucesso":out})
+    }
+)*/
+
+router.delete(
+    "/remover/:id"
+    ,
+    (req,res,next)=>{
+        professorServiceMongo.delete(req,res)
+    }
+)
+
+
+module.exports = router
